5898bd0b4090de41607a6f6b953dd42e6b2a6252
Shiu Kumar <32100156+ShiuKumar@users.noreply.github.com>
2020-11-11 14:19:56 +1300

    Update OPTICAL_plus.m

--- OPTICAL_plus.m
+++ OPTICAL_plus.m
@@ -1,4 +1,4 @@
-function pred_label = OPTICAL_plus(train_data,class_train,test_data,Fs,WindowSize,r)
+function pred_label = OPTICAL_plus(train_data,class_train,test_data,class_test,Fs,WindowSize,r)
 % Fs - sampling frequency
 % train_data - channels x sample_points x n_samples
 % test_data - channels x sample_points x m_samples
@@ -15,7 +15,7 @@ order = 8;
 Train_data = train_data;
 Target_Train = class_train;
 Test_data = test_data;
-
+Target_Test = class_test;
 
 Gbest = get_optimized_par(10, 3, 35,0, Train_data, Target_Train); 
 [AA,BB] = butter(round(Gbest(3)),[Gbest(1) Gbest(2)]/(Fs/2));
@@ -25,15 +25,13 @@ for i=1:length(Target_Train)
 	Train_data(:,:,i) = filter(AA,BB,Train_data(:,:,i)')';
 end
 
-for i=1:length(Target_Test)
+for i=1:size(Test_data,3)
 	Test_data(:,:,i) = filter(AA,BB,Test_data(:,:,i)')';
 end
 
 Target_Train = Target_Train(sum(sum(isnan(Train_data)))==0);
-Target_Test = Target_Test(sum(sum(isnan(Test_data)))==0);
 
 Train_data = Train_data(:,:,sum(sum(isnan(Train_data)))==0);
-Test_data = Test_data(:,:,sum(sum(isnan(Test_data)))==0);
 	
 WS = 128;
 OL = 24;
@@ -45,22 +43,17 @@ for i = 1:length(Target_Train)
 	end
 end
 
-for i = 1:length(Target_Test)
+for i = 1:size(Test_data,3)
 	for j = 1:Nwpt
 		Test_data1(:,:,(i-1)*Nwpt+j) = Test_data(:,(j-1)*OL+1:(j-1)*OL+WS,i);
 		Target_Test1((i-1)*Nwpt+j) = Target_Test(i);
 	end
 end
 
-clear Z
-clear Z1
 [Z, Wcsp] = CSP(Train_data1,Target_Train1',r);
 Wcsp_1 = Wcsp;
 [Z1, Wcsp1] = CSP(Train_data,Target_Train',r);
 Wcsp_2 = Wcsp1;
-
-clear F
-clear F1
 
 for i = 1:1:size(Z,3)
 	var1 = var(Z(:,:,i)');
@@ -72,11 +65,6 @@ for i = 1:1:size(Z1,3)
 	F1(i,:) = log(var1./sum(var1));
 end
 
-clear Z_Test
-clear Z1_Test
-clear F_Test
-clear F1_Test
-
 for i = 1:1:size(Target_Test1,2)
 	Z_Test(:,:,i) = Wcsp*Test_data1(:,:,i); % Z - csp transformed data
 	var1 = var(Z_Test(:,:,i)');
@@ -84,7 +72,7 @@ for i = 1:1:size(Target_Test1,2)
 end
 F_Test_1 = F_Test;
 
-for i = 1:1:length(Target_Test)
+for i = 1:1:size(Test_data,3)
 	Z1_Test(:,:,i) = Wcsp1*real(Test_data(:,:,i)); % Z - csp transformed data
 	var1 = var(Z1_Test(:,:,i)');
 	F1_Test(i,:) = log(var1./sum(var1));
@@ -99,13 +87,11 @@ y2_1 = y2;
 y2_Test = Wlda2'*F1_Test';
 y2_Test_1 = y2_Test;
 
-clear FF_Train
-clear FF_Test
 for i = 1:length(Target_Train)
 	FF_Train{i,1} = F((i-1)*Nwpt+1:i*Nwpt,:)';
 end
 
-for i = 1:length(Target_Test)
+for i = 1:size(Test_data,3)
 	FF_Test{i,1} = F_Test((i-1)*Nwpt+1:i*Nwpt,:)';
 end
 
